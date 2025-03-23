1536acd97dcd3a9304617f54a884bb6401a4cdc7
ELS_Devil <850378753@qq.com>
2025-03-23 21:17:25 +0800

    使用test数据集

--- datasets/incremental.py
+++ datasets/incremental.py
@@ -11,7 +11,7 @@ def generate_cls_order(num_of_cls=90, seed=123):
     cls_order = list(range(1,num_of_cls+1))
     # cls_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
     np.random.shuffle(cls_order)
-    print(len(cls_order))
+    # print(len(cls_order))
     print('creating the training class order...')
     print('current random seed: ' + str(seed))
     print('current class order: ' + str(cls_order))

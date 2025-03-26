0e9c56c99893e48d536d2d8409ffcdd7feb5f1cf
ELS_Devil <850378753@qq.com>
2025-03-26 13:22:48 +0800

    加载断点

--- main.py
+++ main.py
@@ -345,7 +345,7 @@ def main(args):
                 for pg, pg_old in zip(optimizer.param_groups, p_groups):
                     pg['lr'] = pg_old['lr']
                     pg['initial_lr'] = pg_old['initial_lr']
-                print(optimizer.param_groups)
+                # print(optimizer.param_groups) # 打印权重
                 lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
                 args.override_resumed_lr_drop = True
                 if args.override_resumed_lr_drop:
@@ -353,7 +353,7 @@ def main(args):
                     lr_scheduler.step_size = args.lr_drop
                     lr_scheduler.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
                 lr_scheduler.step(lr_scheduler.last_epoch)
-                args.start_epoch = checkpoint['epoch']
+                args.start_epoch = checkpoint['epoch'] + 1
                 print('pretrained weights given...')
                 print('start training base...')
                 for epoch in range(args.start_epoch, args.epochs):
